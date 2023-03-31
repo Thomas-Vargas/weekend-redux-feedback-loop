48dff48c60a19b9e283cbecfa76864585bffa7f0
Thomas-Vargas <tavargas2012irc@gmail.com>
2023-03-31 14:51:34 -0500

    Admin can now delete feedback item.

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -2,6 +2,23 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
+//DELETE
+router.delete('/:id', (req, res) => {
+  const idToDelete = req.params.id;
+  const sqlText = `DELETE FROM "feedback" WHERE "id"=${idToDelete};`
+
+  pool
+    .query(sqlText)
+    .then(result => {
+      res.sendStatus(201);
+      console.log('Successfully delete item with id:', idToDelete);
+    })
+    .catch(error => {
+      res.sendStatus(500);
+      console.log('Error deleting feedback from database', error)
+    })
+})
+
 //GET
 router.get('/', (req, res) => {
   const sqlText = `SELECT * FROM "feedback"`;
