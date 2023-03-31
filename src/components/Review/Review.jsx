a26b876fc5bf644549fa46661f71dd01d2104577
Thomas-Vargas <tavargas2012irc@gmail.com>
2023-03-31 10:55:39 -0500

    Now displaying feedback data in Review component.

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,8 +1,10 @@
 import { Button } from "@mui/material";
 import { useHistory } from "react-router-dom";
+import { useSelector } from 'react-redux';
 
 const Review = () => {
   const history = useHistory();
+  const feedback = useSelector(store => store.feedback)
 
   const handleClick = () => {
     history.push('/success')
@@ -11,10 +13,10 @@ const Review = () => {
   return (
     <div>
       <h1>Review your feedback</h1>
-      <p>Feelings: TEST</p>
-      <p>Understanding: TEST</p>
-      <p>Support: TEST</p>
-      <p>Comments: TEST</p>
+      <p>Feelings: {feedback.feeling}</p>
+      <p>Understanding: {feedback.understanding}</p>
+      <p>Support: {feedback.supported}</p>
+      <p>Comments: {feedback.comment}</p>
       <Button onClick={handleClick} variant="contained">Submit</Button>
     </div>
   )
