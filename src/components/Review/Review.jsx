fc495b41ec37d114db03e3b1c09bc5efdd34fc09
Thomas-Vargas <tavargas2012irc@gmail.com>
2023-03-31 13:30:02 -0500

    Set up post request (axios and router).

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,13 +1,21 @@
 import { Button } from "@mui/material";
 import { useHistory } from "react-router-dom";
 import { useSelector } from 'react-redux';
+import axios from 'axios';
 
 const Review = () => {
   const history = useHistory();
   const feedback = useSelector(store => store.feedback)
 
   const handleClick = () => {
-    history.push('/success')
+    axios.post('/feedback', feedback)
+      .then(response => {
+        console.log(response);
+        history.push('/success')
+      })
+      .catch(error => {
+        console.log(error);
+      })
   }
 
   return (
